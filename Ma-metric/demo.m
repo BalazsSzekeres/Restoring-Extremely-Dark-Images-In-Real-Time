38309637ceef3d63c74e2fa4b6c22dc95613fab5
MasterLuc128 <54933967+MasterLuc128@users.noreply.github.com>
2022-03-31 12:07:56 +0200

    update demo to calculate metrics over all images
    Make a folder in the ma-metric folder and add the images there (jpg)

--- Ma-metric/demo.m
+++ Ma-metric/demo.m
@@ -1,6 +1,22 @@
 
-addpath('external\matlabPyrTools','external\randomforest-matlab\RF_Reg_C');
+addpath('external\matlabPyrTools','external\randomforest-matlab\RF_Reg_C', 'image');
 
-im=imread('peppers.png');
+imagefiles = dir('image\*.jpg');      
+nfiles = length(imagefiles);    % Number of files found
+scoreMAtotal = 0;
+scoreNIQEtotal = 0;
+nfiles
 
-score=quality_predict(im);
+for i=1:nfiles
+   currentfilename = imagefiles(i).name;
+   currentimage = imread(currentfilename);
+   scoreMA{i} = quality_predict(currentimage);
+   scoreNIQE{i} = niqe(currentimage);
+   scoreMAtotal = scoreMAtotal + scoreMA{i};
+   scoreNIQEtotal = scoreNIQEtotal + scoreNIQE{i};
+   images{i} = currentimage;
+   i
+end
+
+scoreMAavg = scoreMAtotal/nfiles;
+scoreNIQEavg = scoreNIQEtotal/nfiles;
